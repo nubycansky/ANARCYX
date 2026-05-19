e9be24d065d1a2f428de30b46a5ca8e68b4b4978
Hahahihiwahaha <muflihasafira@gmail.com>
2026-05-19 16:55:12 +0700

    ini nyoba
    fnaklfhawhfialhflca

--- database/migrations/2026_05_19_070523_create_reptiles_table.php
+++ database/migrations/2026_05_19_070523_create_reptiles_table.php
@@ -9,13 +9,25 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('reptiles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up()
+{
+    Schema::create('reptiles', function (Blueprint $table) {
+        $table->id(); // (1) Otomatis jadi BigInt Primary Key
+        $table->string('name'); // (2) Nama reptil
+        $table->enum('category', ['ular', 'kadal', 'kura-kura', 'lainnya']); // (3) Kategori
+        $table->string('morph_genetics')->nullable(); // (4) Morph (dibikin nullable, jaga-jaga kalau kura-kura biasa ga ada morph-nya)
+        $table->enum('sex', ['jantan', 'betina', 'unsex']); // (5) Jenis kelamin
+        $table->string('age'); // (6) Umur
+        $table->integer('weight'); // (7) Berat (gram)
+        $table->integer('length'); // (8) Panjang (cm)
+        $table->text('description')->nullable(); // (9) Keterangan tambahan (nullable biar aman)
+        $table->text('care_instructions')->nullable(); // (10) Panduan perawatan
+        $table->integer('price'); // (11) Harga
+        $table->integer('stock'); // (12) Stok
+        $table->string('image')->nullable(); // (13) Nama file foto (nullable buat antisipasi foto belum di-upload)
+        $table->timestamps(); // (14) Ini otomatis bikin created_at & updated_at
+    });
+}
 
     /**
      * Reverse the migrations.
